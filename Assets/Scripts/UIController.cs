20b956f1a5e6a348f4e3c7080079f7d51322ba85
IKE37 <maidat3012000@gmail.com>
2023-08-29 18:52:15 +0700

    Player On The Network & Player Health System

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public TMP_Text overheatedMessage;
+    public Slider weaponTempSlider;
+
+    public GameObject deathScreen;
+    public TMP_Text deathText;
+
+    public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
